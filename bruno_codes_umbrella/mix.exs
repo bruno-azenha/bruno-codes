1ba4f690ad9dade8eea09803ee749545efad8801
bruno-azenha <azenha.bruno@gmail.com>
2020-01-27 22:48:23 +0100

    Add release config for umbrella

--- bruno_codes_umbrella/mix.exs
+++ bruno_codes_umbrella/mix.exs
@@ -2,26 +2,19 @@ defmodule BrunoCodes.Umbrella.MixProject do
   use Mix.Project
 
   def project do
-    [
-      apps_path: "apps",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
+    [apps_path: "apps", start_permanent: Mix.env() == :prod, deps: deps(), releases: releases()]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps folder
   defp deps do
     []
+  end
+
+  defp releases do
+    [
+      bruno_codes: [
+        version: "0.1.0",
+        applications: [bruno_codes_web: :permanent]
+      ]
+    ]
   end
 end
